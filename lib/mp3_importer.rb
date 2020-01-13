c029812410bbf0f920cfedeb0ef8ac960ff21852
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-13 19:07:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,11 +9,11 @@ class MP3Importer
   
   def files
     Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
-    end
-  end
+ end
 
  def import
    files.each{|file_name| Song.new_by_filename(file_name)}
   end
+end
   
  
